7b8be58004b30d15081df4980ac50621c830328a
MammNeRF-RPN <100287694+yass123am@users.noreply.github.com>
2025-12-24 15:44:42 +0100

    Update mesh-viewer.js

--- static/js/mesh-viewer.js
+++ static/js/mesh-viewer.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const modelPaths = [
-      './static/mesh/Volume_1.ply',
-      './static/mesh/nerf_3D_grid.ply',
+      './static/mesh/new_v.ply',
+      './static/mesh/new_v.ply',
       './static/mesh/new_v.ply',
     ];
 
